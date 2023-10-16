939d50b5c37eee3589e23d38b39bf62939105f54
PedroMarques0 <pedrofrance02@gmail.com>
2023-10-16 16:53:32 +0200

    first commit

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,25 @@
-function PokemonCard () {
-    return (
-        <figure>
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokémon Bulbasaure" /> 
-          <figcaption>
-            <p>Bulbasaur</p>
-          </figcaption>
-        </figure>
+function PokemonCard() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  const pokemon = pokemonList[0];
+  console.log(pokemon.name);
 
-
-    )
+  return (
+    <figure>
+      <img src={pokemon.imgSrc} alt="" />
+      <figcaption>
+        <p>{pokemon.name}</p>
+      </figcaption>
+    </figure>
+  );
 }
 
 export default PokemonCard;
-
-
-
-
-
